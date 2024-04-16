057f52cb830e5baf4fa9fae4bfef2fa499d80dc6
Ribbit <echo07200503@gmail.com>
2024-04-16 18:37:00 +0900

    🌎 各言語の文章を追加

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -37,18 +37,22 @@ export const ui = {
     'desktop.dialog.title': 'プラグインの設定情報',
   },
   en: {
-    'config.condition.memo.title': 'Memo',
-    'config.condition.memo.description':
-      'This is a sample setting. You can save text information in the plugin.',
-    'config.condition.memo.label': '📝 Memo',
-    'config.condition.memo.placeholder': 'Enter text',
-    'config.condition.field.title': 'Target Field',
-    'config.condition.field.description':
-      'This is a sample setting. It retrieves and displays a list of fields from the app settings.',
-    'config.condition.isSampleUIShown.title': 'Switch Sample',
-    'config.condition.isSampleUIShown.description':
-      'This is a sample switch that toggles between enabled and disabled. Here, it toggles whether to display a sample UI in the record list.',
-    'config.condition.isSampleUIShown.label': 'Show Sample UI',
+    'config.condition.targetFieldCode.title': 'Target Field',
+    'config.condition.targetFieldCode.description': 'Select the field to enable autocomplete',
+    'config.condition.targetFieldCode.label': 'Target Field',
+    'config.condition.srcAppId.title': 'Source App',
+    'config.condition.srcAppId.description':
+      'Select the app to reference for autocomplete suggestions',
+    'config.condition.srcAppId.label': 'Source App',
+    'config.condition.srcFieldCode.title': 'Source Field',
+    'config.condition.srcFieldCode.description':
+      'Select the field to reference for autocomplete suggestions',
+    'config.condition.srcFieldCode.label': 'Source Field',
+    'config.condition.limit.title': 'Display Limit',
+    'config.condition.limit.description':
+      'Set the limit for displaying suggestions. If the number of data to reference is too large, it may affect performance, but setting a limit can help resolve this. This limit only affects the displayed data, and the search will still be performed on all data. If you specify 0, there will be no limit.',
+
+    'config.sidebar.tab.label': 'Settings',
     'config.button.save': 'Save Settings',
     'config.button.return': 'Return to Plugin List',
     'config.toast.save': 'Settings saved',
@@ -58,64 +62,71 @@ export const ui = {
     'config.error.root':
       'The root element does not exist in the plugin HTML. To render the plugin settings, an element with id="settings" is required.',
     'config.error.import': 'Failed to import settings. Please check the file for errors.',
-    'config.error.export':
-      'Failed to export the plugin settings. Please contact the plugin developer.',
+    'config.error.export': 'Failed to export plugin settings. Please contact the plugin developer.',
     'desktop.dialogtrigger.title': 'Plugin is enabled',
     'desktop.dialogtrigger.content': 'Click to view event details',
     'desktop.dialog.title': 'Plugin Settings',
   },
   es: {
-    'config.condition.memo.title': 'Memo',
-    'config.condition.memo.description':
-      'Esta es una configuración de ejemplo. Puede guardar información de texto en el complemento.',
-    'config.condition.memo.label': '📝 Memo',
-    'config.condition.memo.placeholder': 'Ingrese texto',
-    'config.condition.field.title': 'Campo objetivo',
-    'config.condition.field.description':
-      'Esta es una configuración de ejemplo. Recupera y muestra una lista de campos de la configuración de la aplicación.',
-    'config.condition.isSampleUIShown.title': 'Interruptor de muestra',
-    'config.condition.isSampleUIShown.description':
-      'Este es un interruptor de muestra que alterna entre habilitado y deshabilitado. Aquí, alterna si se muestra una interfaz de usuario de muestra en la lista de registros.',
-    'config.condition.isSampleUIShown.label': 'Mostrar interfaz de usuario de muestra',
+    'config.condition.targetFieldCode.title': 'Campo de destino',
+    'config.condition.targetFieldCode.description':
+      'Seleccione el campo para habilitar el autocompletado',
+    'config.condition.targetFieldCode.label': 'Campo de destino',
+    'config.condition.srcAppId.title': 'Aplicación de origen',
+    'config.condition.srcAppId.description':
+      'Seleccione la aplicación de referencia para las sugerencias de autocompletado',
+    'config.condition.srcAppId.label': 'Aplicación de origen',
+    'config.condition.srcFieldCode.title': 'Campo de origen',
+    'config.condition.srcFieldCode.description':
+      'Seleccione el campo de referencia para las sugerencias de autocompletado',
+    'config.condition.srcFieldCode.label': 'Campo de origen',
+    'config.condition.limit.title': 'Límite de visualización',
+    'config.condition.limit.description':
+      'Establezca el límite para mostrar sugerencias. Si el número de datos a referenciar es demasiado grande, puede afectar el rendimiento, pero establecer un límite puede ayudar a resolver esto. Este límite solo afecta los datos mostrados y la búsqueda se realizará en todos los datos. Si especifica 0, no habrá límite.',
+
+    'config.sidebar.tab.label': 'Configuración',
     'config.button.save': 'Guardar configuración',
-    'config.button.return': 'Volver a la lista de complementos',
+    'config.button.return': 'Volver a la lista de plugins',
     'config.toast.save': 'Configuración guardada',
     'config.toast.reset': 'Configuración restablecida',
     'config.toast.import': 'Configuración importada',
-    'config.toast.export': 'Configuración del complemento exportada',
+    'config.toast.export': 'Configuración del plugin exportada',
     'config.error.root':
-      'El elemento raíz no existe en el HTML del complemento. Para renderizar la configuración del complemento, se requiere un elemento con id="settings".',
+      'El elemento raíz no existe en el HTML del plugin. Para renderizar la configuración del plugin, se requiere un elemento con id="settings".',
     'config.error.import':
-      'Error al importar la configuración. Por favor, verifique el archivo en busca de errores.',
+      'Error al importar la configuración. Por favor, verifique que el archivo no contenga errores.',
     'config.error.export':
-      'Error al exportar la configuración del complemento. Por favor, contacte al desarrollador del complemento.',
-    'desktop.dialogtrigger.title': 'El complemento está habilitado',
+      'Error al exportar la configuración del plugin. Por favor, contacte al desarrollador del plugin.',
+    'desktop.dialogtrigger.title': 'El plugin está habilitado',
     'desktop.dialogtrigger.content': 'Haz clic para ver los detalles del evento',
-    'desktop.dialog.title': 'Configuración del complemento',
+    'desktop.dialog.title': 'Configuración del plugin',
   },
   zh: {
-    'config.condition.memo.title': '备忘录',
-    'config.condition.memo.description': '这是一个示例设置。您可以在插件中保存文本信息。',
-    'config.condition.memo.label': '📝 备忘录',
-    'config.condition.memo.placeholder': '输入文本',
-    'config.condition.field.title': '目标字段',
-    'config.condition.field.description':
-      '这是一个示例设置。它从应用程序设置中检索并显示字段列表。',
-    'config.condition.isSampleUIShown.title': '开关示例',
-    'config.condition.isSampleUIShown.description':
-      '这是一个示例开关，可在启用和禁用之间切换。在这里，它切换是否在记录列表中显示示例UI。',
-    'config.condition.isSampleUIShown.label': '显示示例UI',
+    'config.condition.targetFieldCode.title': '目标字段',
+    'config.condition.targetFieldCode.description': '请选择要启用自动完成的字段',
+    'config.condition.targetFieldCode.label': '目标字段',
+    'config.condition.srcAppId.title': '参考应用',
+    'config.condition.srcAppId.description': '请选择用于自动完成建议的参考应用',
+    'config.condition.srcAppId.label': '参考应用',
+    'config.condition.srcFieldCode.title': '参考字段',
+    'config.condition.srcFieldCode.description': '请选择用于自动完成建议的参考字段',
+    'config.condition.srcFieldCode.label': '参考字段',
+    'config.condition.limit.title': '显示限制',
+    'config.condition.limit.description':
+      '设置显示建议的限制。如果要参考的数据数量过大，可能会影响性能，但设置限制可以解决此问题。此限制仅影响显示的数据，搜索仍将在所有数据上执行。如果指定为0，则没有限制。',
+
+    'config.sidebar.tab.label': '设置',
     'config.button.save': '保存设置',
     'config.button.return': '返回插件列表',
     'config.toast.save': '设置已保存',
     'config.toast.reset': '设置已重置',
     'config.toast.import': '已导入设置',
     'config.toast.export': '已导出插件设置',
-    'config.error.root': '插件HTML中不存在根元素。要渲染插件设置，需要一个id="settings"的元素。',
+    'config.error.root': '插件的HTML中不存在根元素。要渲染插件设置，需要一个id="settings"的元素。',
     'config.error.import': '导入设置失败。请检查文件是否有错误。',
     'config.error.export': '导出插件设置失败。请联系插件开发者。',
     'desktop.dialogtrigger.title': '插件已启用',
-    'desktop.dialogtrigger.content': '点击查看事件详情',
+    'desktop.dialogtrigger.content': '单击以查看事件详细信息',
     'desktop.dialog.title': '插件设置',
   },
 } as const;
